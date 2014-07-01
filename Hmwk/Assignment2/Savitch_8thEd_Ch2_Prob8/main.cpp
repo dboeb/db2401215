0a807965496c8f529f6b6f9b60aa1e279e4df577
dboeb <dboebinger@student.rcc.edu>
2014-07-01 14:48:49 -0700

    Signed-off-by: dboeb <dboebinger@student.rcc.edu>

--- Hmwk/Assignment2/Savitch_8thEd_Ch2_Prob8/main.cpp
+++ Hmwk/Assignment2/Savitch_8thEd_Ch2_Prob8/main.cpp
@@ -3,6 +3,7 @@
  * Author: Daniel Boebinger
  * Created on June 30, 2014, 4:35 PM
  */
+
 //System Libraries
 #include <iostream>
 #include <iomanip>
@@ -17,22 +18,29 @@ using namespace std;
 //Execution Starts Here!
 int main(int argc, char** argv) {
     //Declare variables
-    float cost,//(known/input)cost of the item(s) in dollars
-          years,//(known/input)number of years from now that the item will be purchased
-          inflate,//(known/input)the rate of inflation in percent per year
-          newcost;//(unknown)estimated price after inflation in dollars
-    //Input current cost, years to purchase, and rate of inflation
+    //Inputs
+    float cost;   //)cost of the item(s) in dollars
+    float years;  //number of years from now that the item will be purchased
+    float inflate;//the rate of inflation in percent per year
+    //Outputs
+    float newcost;//estimated price after inflation in dollars
+    
+    //Input Values      
     cout<<"Enter the cost of the item(s) in dollars:"<<endl;
     cin>>cost;
     cout<<"Enter the number of years from now"<<endl<<"that the item will be purchased:"<<endl;
     cin>>years;
     cout<<"Enter the rate of inflation in percent per year:"<<endl;
     cin>>inflate;
+    
+    //Calculations
     //Calculate the estimated price after inflation
     newcost=cost+cost*years*(inflate/100);
-    //Output the estimated price of item(s)
+    
+    //Output the results
     cout<<setprecision(2)<<fixed<<showpoint;
     cout<<"Estimated cost of item(s)"<<endl<<"due to inflation = $"<<newcost;
+    
     //Exit stage right
     return 0;
 }
